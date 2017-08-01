4d1f94b835ebd681ae08c1f3d657ce03be6ec1d5
Bobby90622 <419237106@qq.com>
2017-08-01 21:45:12 +0800

    2017.8.1 第一波
    修改框架结构

---
+++ Index/Runtime/Cache/f9c22b8b40e560036663a0ce7ed378ae.php
@@ -0,0 +1,36 @@
+<?php if (!defined('THINK_PATH')) exit();?><!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>达令</title>
+	</head>
+	<body>
+		<div class="navigator">
+			
+<header class="header">
+	<div class="top">
+		<div class="consumerInfo">
+			<a href="" class="consumerInfo">消费者告知书</a>
+		</div>
+		<ul class="loginBar">
+			<li class="login">
+				<a href="#">登陆</a>
+			</li>
+			<li class="register">
+				<a href="#">注册</a>
+			</li>
+			<li class="tel">
+				<a href="#">400-080-1888</a>
+			</li>
+			<li class="download">
+				<a href="#">下载达令app</a>
+			</li>
+		</ul>
+	</div>
+	<div class="center"></div>
+	<div class="bottom"></div>
+</header>
+
+		</div>
+	</body>
+</html>
