41208ed07bb2a7bdb4c8db20debc7549ca4ecfb7
497955191@qq.com <497955191@qq.com>
2017-08-02 22:17:48 +0800

    商品简介
    商品简介

--- Index/Runtime/Cache/c9fb3b7e8b43c6f8d5fc3f6661bf30ee.php
+++ Index/Runtime/Cache/c9fb3b7e8b43c6f8d5fc3f6661bf30ee.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<?php if (!defined('THINK_PATH')) exit();?><!doctype html>
 <html>
 	<head>
 		<meta charset="utf-8" />
@@ -12,7 +12,83 @@
 	</head>
 	<body>
 		<div class="navigator">
-			<include file='Public:header' />
+			<header class="header">
+	<div class="top">
+		<div class="topWrap">
+			<div class="consumerInfo">
+				<a href="#">消费者告知书</a>
+			</div>
+			<ul class="loginBar">
+				<li class="login">
+					<a href="#">登陆</a>
+				</li>
+				<li class="register">
+					<a href="#">注册</a>
+				</li>
+				<li class="tel">
+					<img src="__ROOT__/Index/Common/images/header/tel.svg" alt="" />
+					<a >400-080-1888</a>
+				</li>
+				<li class="download">
+					<a href="#">下载达令app</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="center">
+		<div class="centerWrap">
+			<a href="#" class="logo"></a>
+			<form action="" method='get' class="searchForm">
+				<div class="searchBar">
+					<div class="searchBox">
+						<input type="hidden" />
+						<input type="hidden" />
+						<input type="text" class="searchKeyWords" />
+						<input type="submit" value="搜索" class="searchSub" />
+					</div>
+					<ul class="searchHot">
+						<li><a href="#">口红</a></li>
+						<li><a href="#">面膜</a></li>
+						<li><a href="#">耳饰</a></li>
+						<li><a href="#">日系美妆</a></li>
+						<li><a href="#">化妆水</a></li>
+						<li><a href="#">底妆</a></li>
+						<li><a href="#">乳霜</a></li>
+						<li><a href="#">洁面</a></li>
+						<li><a href="#">防嗮</a></li>
+					</ul>
+				</div>
+				<div class="trolley">
+					<a href="#">我的购物车</a>
+					<span>0</span>
+				</div>
+			</form>
+		</div>
+	</div>
+	<div class="bottom">
+		<div class="bottomWrap">
+			<div class="category">
+				<p class="title">
+					全部分类
+				</p>
+				<div class="categoryDetail">
+					
+				</div>
+
+			</div>
+			<ul class="naviBar">
+				<li><a href="#">首页</a></li>
+				<li><a href="#">极致美护</a></li>
+				<li><a href="#">全球零食</a></li>
+				<li><a href="#">创意生活</a></li>
+				<li><a href="#">时尚配饰</a></li>
+				<li><a href="#">尖货女装</a></li>
+				<li><a href="#">3C家电</a></li>
+			</ul>
+		</div>
+	</div>
+</header>
+
 		</div>
 		
 		
@@ -133,10 +209,121 @@
 		
 		
 		<div class="asideBar">
-			<include file='Public:aside' />
+			<aside class="aside">
+	<div class="aside_nav">
+		<!--购物车-->
+		<div class="aside_shopp" onclick="shopp()" >
+			<div class="aside_wire"></div>
+			<div class="aside_car"></div>
+			<div class="aside_text">购<br />物<br />车</div>
+			<div class="aside_number">2</div>
+		</div>
+		<!--横线-->
+		<div class="aside_wire"></div>
+		<!--钱图标-->
+		<div class="aside_money" onclick="money()">
+			<span></span>
+			<strong class="aside_money_text aside_common">我的优惠劵<em></em></strong>
+		</div>
+		<!--收藏-->
+		<div class="aside_collect" onclick="collect()">
+			<span></span>
+			<strong class="aside_collect_text aside_common">我的收藏<em></em></strong>
+		</div>
+		<!--top图-->
+		<a href="#">
+			<img src="" alt="" />
+		</a>
+	</div>
+	<!--购物车里面的内容-->
+	<div class="aside_content">
+		<!--叉-->
+		<div class="aside_remove" onclick="remove()"></div>
+		<div class="aside_car_content">
+			<div class="head">我的购物车</div>
+			<div class="cart-mini-ct">
+				<div class="aside_content_table"></div>
+			</div>
+		</div>
+	</div>
+	<!--钱里面的内容-->
+	<div class="aside_money_content">
+		<div class="head_common">我的优惠劵</div>
+		<div class="sb-unlogin">
+			<p>登录后才能查看</p>
+			<button>登录</button>
+		</div>
+	</div>
+	<!--收藏里面的内容-->
+	<div class="aside_collect_content">
+		<div class="head_common">我的收藏</div>
+		<div class="sb-unlogin">
+			<p>登录后才能查看</p>
+			<button>登录</button>
+		</div>
+	</div>
+</aside>
+
 		</div>
 		<div class="footer">
-			<include file='Public:footer' />
+			
+<footer class="container">
+	<div class="footer title">
+		<ul class="footer list">
+			<li>
+				<span>为什么选择达令</span>
+				<div class="icon_1"></div>
+			</li>
+			<li>
+				
+				<span class="icon_2"></span>
+				<span>100多个品牌授权</span>
+			</li>
+			<li>
+				<span class="icon_3"></span>
+				<span>24小时内发货</span>
+			</li>
+			<li>
+				<span class="icon_4"></span>
+				<span>保税区正品</span>
+			</li>
+		</ul>
+	</div>
+	<div class="footer middle">
+		<div class="middleLeft">
+			<p class="textTop">投资人说达令</p>
+			<p class="textContent">“hi,达令，我是鹿晗，达令投资人，投资比影响力更重要的是责任。”</p>
+		</div>
+		<div class="middleRight">
+			<p class="textTop">投资人说达令</p>
+			<p class="textContent">“达令™一手全球好货--带您购全球,教您神马值得买。<br/>
+连续两年荣获App store 编辑推荐最佳购物类应用！”</p>
+		</div>
+		
+	</div>
+	<div class="footer bottom">
+		<div class="bottomLink">
+			<a href="">首页</a>&nbsp;&nbsp; |
+			<a href="">网站地图</a>&nbsp;&nbsp; |
+			<a href="">消费者告知书</a>&nbsp;&nbsp; |
+			<a href="">优惠券使用规则 </a>&nbsp;&nbsp; |
+			<a href="">退换货使用说明 </a>&nbsp;&nbsp; |
+			<a href="">关于我们</a>&nbsp;&nbsp; |
+			<a href="">联系我们</a>&nbsp;&nbsp; |	
+		</div>
+		<div class="bottomSafe">
+			<span class="info">北京普缇客科技有限公司</span>
+			<a href=""><img class="cert" src="__ROOT__/Index/Common/images/footer/cert.png" alt="" /></a>
+			<a href=""><img class="cnnic" src="__ROOT__/Index/Common/images/footer/cnnic.png" alt="" /></a>
+			<a href=""><img class="zfb" src="__ROOT__/Index/Common/images/footer/ico_zfb.jpg" alt="" /></a>	
+		</div>
+		<div class="bottomMessage">
+			<p>Copyright © 2016达令 ，All Rights Reserved │ 京ICP备14009583号-1 │ 京公网安备11010502027399 | 食品流通许可证编号：SP1101051510323113</p>
+		</div>
+		
+	</div>
+</footer>
+
 		</div>
 	</body>
 	<script type="text/javascript" src="__ROOT__/Index/common/js/aside.js"></script>
