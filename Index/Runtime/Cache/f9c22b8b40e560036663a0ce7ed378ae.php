b8c7ca7e48b1a79d684f4b117ca92c0574c6f5d3
Bobby90622 <419237106@qq.com>
2017-08-02 10:30:46 +0800

    修改权限

--- Index/Runtime/Cache/f9c22b8b40e560036663a0ce7ed378ae.php
+++ Index/Runtime/Cache/f9c22b8b40e560036663a0ce7ed378ae.php
@@ -36,13 +36,29 @@
 		<div class="centerWrap">
 			<a href="#" class="logo"></a>
 			<form action="" method='get'>
-				<div class="searchBox">
-					
+				<div class="searchBar">
+					<div class="searchBox">
+						<input type="hidden" />
+						<input type="hidden" />
+						<input type="text" />
+						<input type="submit" value="搜索" />
+					</div>
+					<ul class="searchHot">
+						<li><a href="#">口红</a></li>
+						<li><a href="#">面膜</a></li>
+						<li><a href="#">耳饰</a></li>
+						<li><a href="#">日系美妆</a></li>
+						<li><a href="#">化妆水</a></li>
+						<li><a href="#">底妆</a></li>
+						<li><a href="#">乳霜</a></li>
+						<li><a href="#">洁面</a></li>
+						<li><a href="#">防嗮</a></li>
+					</ul>
 				</div>
-				<input type="hidden" />
-				<input type="hidden" />
-				<input type="text" />
-				<input type="submit" />
+				<div class="trolley">
+					<a href="#">我的购物车</a>
+					<span>0</span>
+				</div>
 			</form>
 		</div>
 	</div>
@@ -52,62 +68,62 @@
 		</div>
 		<div class="footer">
 			
-		<footer class="container">
-			<div class="footer title">
-				<ul class="footer list">
-					<li>
-						<span>为什么选择达令</span>
-						<div class="icon_1"></div>
-					</li>
-					<li>
-						
-						<span class="icon_2"></span>
-						<span>100多个品牌授权</span>
-					</li>
-					<li>
-						<span class="icon_3"></span>
-						<span>24小时内发货</span>
-					</li>
-					<li>
-						<span class="icon_4"></span>
-						<span>保税区正品</span>
-					</li>
-				</ul>
-			</div>
-			<div class="footer middle">
-				<div class="middleLeft">
-					<p class="textTop">投资人说达令</p>
-					<p class="textContent">“hi,达令，我是鹿晗，达令投资人，投资比影响力更重要的是责任。”</p>
-				</div>
-				<div class="middleRight">
-					<p class="textTop">投资人说达令</p>
-					<p class="textContent">“达令™一手全球好货--带您购全球,教您神马值得买。<br/>
+<footer class="container">
+	<div class="footer title">
+		<ul class="footer list">
+			<li>
+				<span>为什么选择达令</span>
+				<div class="icon_1"></div>
+			</li>
+			<li>
+				
+				<span class="icon_2"></span>
+				<span>100多个品牌授权</span>
+			</li>
+			<li>
+				<span class="icon_3"></span>
+				<span>24小时内发货</span>
+			</li>
+			<li>
+				<span class="icon_4"></span>
+				<span>保税区正品</span>
+			</li>
+		</ul>
+	</div>
+	<div class="footer middle">
+		<div class="middleLeft">
+			<p class="textTop">投资人说达令</p>
+			<p class="textContent">“hi,达令，我是鹿晗，达令投资人，投资比影响力更重要的是责任。”</p>
+		</div>
+		<div class="middleRight">
+			<p class="textTop">投资人说达令</p>
+			<p class="textContent">“达令™一手全球好货--带您购全球,教您神马值得买。<br/>
 连续两年荣获App store 编辑推荐最佳购物类应用！”</p>
-				</div>
-				
-			</div>
-			<div class="footer bottom">
-				<div class="bottomLink">
-					<a href="">首页</a>&nbsp;&nbsp; |
-					<a href="">网站地图</a>&nbsp;&nbsp; |
-					<a href="">消费者告知书</a>&nbsp;&nbsp; |
-					<a href="">优惠券使用规则 </a>&nbsp;&nbsp; |
-					<a href="">退换货使用说明 </a>&nbsp;&nbsp; |
-					<a href="">关于我们</a>&nbsp;&nbsp; |
-					<a href="">联系我们</a>&nbsp;&nbsp; |	
-				</div>
-				<div class="bottomSafe">
-					<span class="info">北京普缇客科技有限公司</span>
-					<a href=""><img class="cert" src="__ROOT__/Index/Common/images/footer/cert.png" alt="" /></a>
-					<a href=""><img class="cnnic" src="__ROOT__/Index/Common/images/footer/cnnic.png" alt="" /></a>
-					<a href=""><img class="zfb" src="__ROOT__/Index/Common/images/footer/ico_zfb.jpg" alt="" /></a>	
-				</div>
-				<div class="bottomMessage">
-					<p>Copyright © 2016达令 ，All Rights Reserved │ 京ICP备14009583号-1 │ 京公网安备11010502027399 | 食品流通许可证编号：SP1101051510323113</p>
-				</div>
-				
-			</div>
-		</footer>
+		</div>
+		
+	</div>
+	<div class="footer bottom">
+		<div class="bottomLink">
+			<a href="">首页</a>&nbsp;&nbsp; |
+			<a href="">网站地图</a>&nbsp;&nbsp; |
+			<a href="">消费者告知书</a>&nbsp;&nbsp; |
+			<a href="">优惠券使用规则 </a>&nbsp;&nbsp; |
+			<a href="">退换货使用说明 </a>&nbsp;&nbsp; |
+			<a href="">关于我们</a>&nbsp;&nbsp; |
+			<a href="">联系我们</a>&nbsp;&nbsp; |	
+		</div>
+		<div class="bottomSafe">
+			<span class="info">北京普缇客科技有限公司</span>
+			<a href=""><img class="cert" src="__ROOT__/Index/Common/images/footer/cert.png" alt="" /></a>
+			<a href=""><img class="cnnic" src="__ROOT__/Index/Common/images/footer/cnnic.png" alt="" /></a>
+			<a href=""><img class="zfb" src="__ROOT__/Index/Common/images/footer/ico_zfb.jpg" alt="" /></a>	
+		</div>
+		<div class="bottomMessage">
+			<p>Copyright © 2016达令 ，All Rights Reserved │ 京ICP备14009583号-1 │ 京公网安备11010502027399 | 食品流通许可证编号：SP1101051510323113</p>
+		</div>
+		
+	</div>
+</footer>
 
 		</div>
 	</body>
