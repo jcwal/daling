86345da1580a9c595130d344cfe6876d1c44f0c9
497955191@qq.com <497955191@qq.com>
2017-08-02 15:18:18 +0800

    权限修改
    权限修改

---
+++ Index/Lib/Action/ProductDetailAction.class.php
@@ -0,0 +1,10 @@
+<?php
+// 本类由系统自动生成，仅供测试用途
+class ProductDetailAction extends Action {
+	public function index(){
+		$this->display('productDetail');
+	}
+//	public function item(){
+//		echo 111;
+//	}
+}
