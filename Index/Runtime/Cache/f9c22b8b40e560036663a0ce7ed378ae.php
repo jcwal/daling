3cfb7facd0df0397e25f92502c62b3744a9257a1
Bobby90622 <419237106@qq.com>
2017-08-02 09:24:39 +0800

    2017.8.2 第二波
    提交header 第一次

--- Index/Runtime/Cache/f9c22b8b40e560036663a0ce7ed378ae.php
+++ Index/Runtime/Cache/f9c22b8b40e560036663a0ce7ed378ae.php
@@ -3,29 +3,33 @@
 	<head>
 		<meta charset="utf-8" />
 		<title>达令</title>
+		<link rel="stylesheet" href="__ROOT__/Index/common/css/reset.css" />
+		<link rel="stylesheet" href="__ROOT__/Index/common/css/header.css" />
 	</head>
 	<body>
 		<div class="navigator">
-			
-<header class="header">
+			<header class="header">
 	<div class="top">
-		<div class="consumerInfo">
-			<a href="" class="consumerInfo">消费者告知书</a>
+		<div class="topWrap">
+			<div class="consumerInfo">
+				<a href="#">消费者告知书</a>
+			</div>
+			<ul class="loginBar">
+				<li class="login">
+					<a href="#">登陆</a>
+				</li>
+				<li class="register">
+					<a href="#">注册</a>
+				</li>
+				<li class="tel">
+					<img src="__ROOT__/Index/Common/images/header/tel.svg" alt="" />
+					<a >400-080-1888</a>
+				</li>
+				<li class="download">
+					<a href="#">下载达令app</a>
+				</li>
+			</ul>
 		</div>
-		<ul class="loginBar">
-			<li class="login">
-				<a href="#">登陆</a>
-			</li>
-			<li class="register">
-				<a href="#">注册</a>
-			</li>
-			<li class="tel">
-				<a href="#">400-080-1888</a>
-			</li>
-			<li class="download">
-				<a href="#">下载达令app</a>
-			</li>
-		</ul>
 	</div>
 	<div class="center"></div>
 	<div class="bottom"></div>
