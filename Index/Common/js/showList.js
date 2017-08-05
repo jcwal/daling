0810a6aa2f649bab9a42fcea06281827de1b9f74
Sky-CCDD <lanou@xuchengdi.local>
2017-08-05 11:56:19 +0800

    showListSubmit(0805)
    showListSubmit

--- Index/Common/js/showList.js
+++ Index/Common/js/showList.js
@@ -6,28 +6,37 @@ $(document).ready(function(){
 		$(".proMenuList").css(
 			"display",'block',
 			)
-		// 点击列表中内容，下拉列表消失，框中的字换成点击选中内容
-		$(".single").click(function(){
-			$(".initVal").html($(this).html());
-			$(".proMenuList").css(
-			"display",'none',
-			)
-  		})
 	});
+  // 点击列表中内容，下拉列表消失，框中的字换成点击选中内容
+	$("li.single").click(function(){
+		$(".initVal").html($(this).html());
+		$(".proMenuList").css(
+		"display",'none',
+		)
+		})
+  // 点击多选按钮，当前列消失，多选项display：block,
   $(".selectMore").click(function(){
-  	// if()
-  	// 	$(this).siblings("a").css(
-  	// 		'display','none',
-  	// 		),
-  	// 	$(this).prev("dd").css(
-  	// 		'display','block',
-  	// 		),
-  	// 	$(this).next("button").css(
-  	// 		'display','none',
-  	// 		)
   		$(this).siblings("a").toggle();
   		$(this).next("button").toggle();
   		$(this).prev("dd").toggle();
+  		// 确定，取消键display:block(点击更多按钮时这两个节点的display:none)
+  		$(this).prev("dd").children(".proMul").css(
+  			'display','block',
+  			);
+  });
+  // 点击更多按钮，多选框display:block同时移除所有input
+  $(".listMore").click(function(){
+  	$(this).html("收起");
+  	$(this).siblings("dd").toggle();
+  	$(this).siblings("dd").children("label").children("input").remove();
+  	$(this).siblings("dd").children(".proMul").css(
+  		'display','none',
+  		);
   })
 });
+ var app=angular.module("myapp",[]);
+ app.controller("myctrl",function($scope){
+  	
+ })
+  		
 
