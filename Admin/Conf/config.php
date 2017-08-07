b6300e70635e3e4614e468b1927a0d91aa8bf3b2
Bobby90622 <419237106@qq.com>
2017-08-07 11:15:06 +0800

    Add favicon and admin.php
    添加favicon 和管理者的主入口

---
+++ Admin/Conf/config.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+	//'配置项'=>'配置值'
+	'DB_TYPE'   => 'mysql', // 数据库类型
+	'DB_HOST'   => 'bdm274886557.my3w.com', // 服务器地址
+	'DB_NAME'   => 'bdm274886557_db', // 数据库名
+	'DB_USER'   => 'bdm274886557', // 用户名
+	'DB_PWD'    => 'lh622623', // 密码
+	'DB_PORT'   => 3306, // 端口
+	'DB_PREFIX' => 'dl_',//前缀
+);
+
+?>
