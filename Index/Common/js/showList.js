1d25b288f96faf9a3696dcc16cc92c166758e816
Sky-CCDD <lanou@xuchengdi.local>
2017-08-11 22:36:47 +0800

    bug修复

--- Index/Common/js/showList.js
+++ Index/Common/js/showList.js
@@ -1,16 +1,75 @@
+
+var paramter = window.location.search;
+var keywords = decodeURI(paramter.substring(paramter.indexOf('=')+1, paramter.indexOf('&')));
 if(app == ''){
     app=angular.module("myapp",[]); 
 };
  app.controller("myctrl",function($scope,$http){
+    // 商品列表
+   $scope.keywords=keywords;
     
-    // $http.get("__APP__/Index/showList").success(function(list){
-    //   $scope.list=list;
+    $http({
+      url:"ProductList/check",
+      method:"POST",
+      data:{
+        keywords:$scope.keywords,
+      },
+      headers:{"Content-type":"application/x-www-form-urlencoded"}
+    }).success(function(data){
+      $scope.list=data;
+      // 分页总数
+      $scope.pageSize=4;
+
+      $scope.pages=Math.ceil($scope.list.length/$scope.pageSize);  //根据返回的数据分得的总页数 
+      $scope.newPages=$scope.pages>4?4:$scope.pages;
+      $scope.pageList=[];
+      // 分页起始页
+      $scope.selPage=1;
+      $scope.setData=function(){
+        // 每页展示的条目
+        $scope.items=$scope.list.slice(($scope.pageSize*($scope.selPage-1)),($scope.selPage*$scope.pageSize));
+      }
+      //根据关键字查库成功返回数据时每页展示的条目
+      $scope.items=$scope.list.slice(0,$scope.pageSize);
+      for(var i=0;i<$scope.newPages;i++){
+        $scope.pageList.push(i+1);
+      }
+      $scope.selectPage=function(page){
+        if(page<1 || page>$scope.pages) return;
+        // 最多分页数4；
+        if(page>2){
+          var newpageList=[];
+          for(var i=(page-2);i<((page+2)>$scope.pages?$scope.pages:(page+2));i++){
+            newpageList.push(i+1);
+        }
+        $scope.pageList=newpageList;
+        }
+        $scope.selPage = page;
+        $scope.setData();
+        $scope.isActivePage(page);
+        console.log("选择的页：" + page);
+      };
+       //设置当前选中页样式
+        $scope.isActivePage = function (page) {
+            return $scope.selPage == page;
+        };
+        //上一页
+        $scope.First = function () {
+            $scope.selectPage($scope.selPage - 1);
+        }
+        //下一页
+        $scope.Next = function () {
+            $scope.selectPage($scope.selPage + 1);
+        };
+    })
+
+    
 
 
-    // })
-    
+
+
  });
-  // }
+
 $(document).ready(function(){
 	// 点击极致美护框下拉列表呈现
   $("#proMenu").click(function(){
--- Index/Lib/Action/ProductListAction.class.php
+++ Index/Lib/Action/ProductListAction.class.php
@@ -5,21 +5,16 @@ class ProductListAction extends Action {
 		$this->display('showList');
 	}
 	// 商品列表页面
-	function showList(){
-		// var paramter = window.location.search;
-		// var keywords = paramter.substring(paramter.IndexOf('=')+1, paramter.length);
-		$product=M('product');
-		$list=$product->where('keywords=="极致美护"')->select();
-		var_dump($list);die;
-		// if($list){
-		// 	$data['info']="数据查询成功";
-		// 	$data['status']=1;
-		// }else{
-		// 	$data['info']='数据查询失败';
-		// 	$data['status']=0;
-		// }
-		$this->ajaxReturn($list);		
+	function check(){
+		$data=file_get_contents("php://input");
+		$checkData=json_decode($data,true);
+		$product=M("product");
+		$keyData=$checkData['keywords'];
+		$where['keywords']=array('like',"%{$keyData}%");
+		$list=$product->where($where)->select();
+		$this->ajaxReturn($list);
 	}
+	
 	
 	
 }
